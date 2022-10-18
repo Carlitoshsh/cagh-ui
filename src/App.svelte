1e185bd7b44ce8080fe12e73868504bf97c44928
Carlos Gómez <caragomezher@unal.edu.co>
2022-10-17 22:58:07 -0500

    Add tree

--- src/App.svelte
+++ src/App.svelte
@@ -1,172 +1,107 @@
 <script>
-  import { onMount } from "svelte";
-  let pokemon = [];
-  let currentPage = 1;
-  let totalPages = 0;
-  let totalResults = 0;
-  let next = "";
-  let previous = "";
-  let limit = 10;
-  let offset = 0;
-  let paginationSpaces = 4;
+  import TreeItem from "./TreeItem.svelte";
 
-  onMount(() => {
-    getPokemonByLimit(limit, offset);
-  });
 
-  function getPokemonByLimit(lim, off) {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${lim}&offset=${off}`)
-      .then((response) => response.json())
-      .then((data) => {
-        pokemon = data.results;
-        currentPage = Math.floor(off / lim) + 1;
-        totalPages = Math.ceil(data.count / lim);
-        totalResults = data.count;
-        next = data.next;
-        previous = data.previous;
-        offset = off;
-      });
-  }
+  const dataTree = [
+    {
+      id: 1,
+      name: 'Root',
+      children: [
+        {
+          id: 2,
+          name: 'Child 1',
+          children: [
+            {
+              id: 3,
+              name: 'Child 1.1',
+              children: [
+                {
+                  id: 4,
+                  name: 'Child 1.1.1',
+                  children: []
+                }
+              ]
+            },
+            {
+              id: 5,
+              name: 'Child 1.2',
+              children: []
+            },
+            {
+              id: 6,
+              name: 'Child 1.3',
+              children: []
+            }
+          ]
+        },
+        {
+          id: 15,
+          name: 'Child 2',
+          children: []
+        },
+        {
+          id: 16,
+          name: 'Child 3',
+          children: []
+        }
+      ]
+    },
+    {
+      id: 7,
+      name: 'Root 2',
+      children: [
+        {
+          id: 8,
+          name: 'Child 1',
+          children: [
+            {
+              id: 9,
+              name: 'Child 1.1',
+              children: [
+                {
+                  id: 10,
+                  name: 'Child 1.1.1',
+                  children: []
+                }
+              ]
+            },
+            {
+              id: 11,
+              name: 'Child 1.2',
+              children: []
+            },
+            {
+              id: 12,
+              name: 'Child 1.3',
+              children: []
+            }
+          ]
+        },
+        {
+          id: 13,
+          name: 'Child 2',
+          children: []
+        },
+        {
+          id: 14,
+          name: 'Child 3',
+          children: []
+        }
+      ]
+    }
+  ];
 </script>
 
 <main>
-  <div>
-    <h1>Pokemon</h1>
-    <div>
-      {#each pokemon as poke}
-        <div>
-          <h2>{poke.name}</h2>
-        </div>
-      {/each}
-    </div>
-    <div>
-      totalPages: {totalPages}
-      <button
-        on:click={() => {
-          getPokemonByLimit(limit, offset - limit);
-        }}
-        disabled={currentPage === 1}
-      >
-        Previous
-      </button>
-      <button
-        on:click={() => {
-          getPokemonByLimit(limit, offset + limit);
-        }}
-        disabled={currentPage === totalPages}
-      >
-        Next
-      </button>
+  {#each dataTree as node}
+    <TreeItem {node} />
+  {/each}
 
-      <h2>Pagination</h2>
-      <div>
-        <button
-          on:click={() => {
-            getPokemonByLimit(limit, 0);
-          }}
-          disabled={currentPage === 1}
-        >
-          First
-        </button>
-        <button
-          on:click={() => {
-            getPokemonByLimit(limit, (totalPages - 1) * limit);
-          }}
-          disabled={currentPage === totalPages}
-        >
-          Last
-        </button>
-      </div>
-      {{ currentPage }} of {{ totalPages }}
-      <div>
-        {#each Array.from({ length: paginationSpaces }) as _, i}
-          {#if currentPage - paginationSpaces + i > 0}
-            <button
-              on:click={() => {
-                getPokemonByLimit(
-                  limit,
-                  (currentPage - paginationSpaces + i - 1) * limit
-                );
-              }}
-              disabled={currentPage === currentPage - paginationSpaces + i}
-            >
-              {currentPage - paginationSpaces + i}
-            </button>
-          {:else if currentPage + i < totalPages && currentPage + i < paginationSpaces}
-            <button
-              on:click={() => {
-                getPokemonByLimit(limit, (currentPage + i - 1) * limit);
-              }}
-              disabled={currentPage === currentPage + i}
-            >
-              {currentPage + i}
-            </button>
-          {/if}
-        {/each}
-      </div>
-    </div>
-  </div>
 </main>
 
-<a href="https://alistapart.com/article/web-typography-tables/">Responsive tables</a>
 
 <style>
   main {
-    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
-
-  .flex-space-between {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .flex-center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    grid-gap: 1rem;
-  }
-
-  .grid-item {
-    /* background-color: #f1f1f1; */
-    padding: 1rem;
-    border-radius: 0.25rem;
-  }
-
-/* table with data header */
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
-
-  th,
-  td {
-    text-align: left;
-    padding: 8px;
-  }
-
-  tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
-
-  th {
-    background-color: #4caf50;
-    color: white;
-  }
-
-  .pagination {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    font-family: 'Courier New', Courier, monospace;
   }
 </style>
---
+++ src/TreeItem.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let node;
+</script>
+
+<details>
+  <summary>{node.name}</summary>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sint,
+    accusamus consectetur doloribus numquam dolorum mollitia tempora. Doloribus
+    incidunt recusandae ipsa maxime, mollitia dolore odio. Pariatur deserunt
+    impedit nesciunt molestiae.
+  </p>
+  {#each node.children as child}
+    <svelte:self node={child} />
+  {/each}
+</details>
+
+<style>
+  details {
+    margin: 1rem;
+    border: 1px solid;
+    padding: 1rem;
+    border-radius: 0.5rem;
+  }
+  summary {
+    cursor: pointer;
+    background-color: black;
+    color: white;
+    padding: 0.5rem;    
+  }
+</style>
